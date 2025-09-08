284bd4691e292d898fae47aca5bb798497e01561
Bassant120240302 <bassantfoe@gmail.com>
2025-09-08 11:07:13 +0300

    Signed-off-by: Bassant120240302 <bassantfoe@gmail.com>

---
+++ session12 java/js/script.js
@@ -0,0 +1,16 @@
+// var degree=prompt('')
+// if (degree==null || degree=='') console.log('Enter your degree')
+//     else{
+//           if (degree>=90 || degree==100) console.log('A')
+//           else if (degree>=80 && degree<90) console.log('B')
+//           else if (degree>=70 && degree<80) console.log('C')
+//           else if (degree>=60 && degree<70) console.log('D')
+//           else if (degree<60) console.log('F')
+//           else console.log('invalid degree try again (0-100)')
+        
+
+for(var i=1; i<=5; i++){
+    var fName=prompt('')
+   if ( fName=='Bassant') break
+   console.log(fName)
+}
